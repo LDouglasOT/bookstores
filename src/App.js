b7a73698195160370c08d289fb743bd8de2a8647
unknown <douglas.luzinda.ot@gmail.com>
2023-05-19 14:32:37 +0300

    redux in components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
+import { Provider } from 'react-redux';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Books from './Pages/Books';
 import Categories from './Pages/Categories';
+import store from './redux/store';
 
 function App() {
   return (
+
     <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Books />} />
-          <Route path="/categories" element={<Categories />} />
-        </Routes>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Books />} />
+            <Route path="/categories" element={<Categories />} />
+          </Routes>
+        </div>
+      </Provider>
     </BrowserRouter>
   );
 }
